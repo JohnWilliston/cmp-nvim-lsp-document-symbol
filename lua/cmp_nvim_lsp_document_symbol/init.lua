3ffe4b3bcaf3451f9f4b10b4aefb5fcc39ccf1f9
JohnWilliston <john@willistons.com>
2024-12-30 21:45:17 -0800

    Added a mapping so the popup window information makes more sense.
    I don't understand why, but the popup window was listing classes as
    fields, methods as variables, functions as values, etc. I forked the
    repo and added a little mapping hack that is admittedly neither correct
    nor complete. But it fixes my issue for the moment and perhaps helps the
    original author find the proper solution.

--- lua/cmp_nvim_lsp_document_symbol/init.lua
+++ lua/cmp_nvim_lsp_document_symbol/init.lua
@@ -29,6 +29,72 @@ local SymbolKind = {
   [26] = 'TypeParameter',
 }
 
+local NewSymbolNames = {
+   [1] =    "Text",
+   [2] =    "Method",
+   [3] =    "Function",
+   [4] =    "Constructor",
+   [5] =    "Field",
+   [6] =    "Variable",
+   [7] =    "Class",
+   [8] =    "Interface",
+   [9] =    "Module",
+   [10] =   "Property",
+   [11] =   "Unit",
+   [12] =   "Value",
+   [13] =   "Enum",
+   [14] =   "Keyword",
+   [15] =   "Snippet",
+   [16] =   "Color",
+   [17] =   "File",
+   [18] =   "Reference",
+   [19] =   "Folder",
+   [20] =   "EnumMember",
+   [21] =   "Constant",
+   [22] =   "Struct",
+   [23] =   "Event",
+   [24] =   "Operator",
+   [25] =   "TypeParameter",
+   [26] =   "(Unknown)",
+}
+
+-- I don't know why this is needed, but for some reason the search results
+-- come back with the wrong icons for what they are. The original table above,
+-- named `SymbolKind` seems to be what the plugin is getting, and correctly at
+-- that. But then the number that comes in (e.g., 5 for classes) then gets
+-- incorrectly described in the resulting popup as a field. So I added the new
+-- symbol names, which I copied from another working plugin, and established
+-- a mapping table as best I could to take the input number and map it to are
+-- proper output number.
+local kindMapping = {
+    [1] = 11,   -- File to Unit
+    [2] = 9,
+    [3] = 0,    -- I don't know what to do with Namespace
+    [4] = 0,    -- Package to Module
+    [5] = 7,
+    [6] = 2,
+    [7] = 10,
+    [8] = 5,
+    [9] = 4,
+    [10] = 13,
+    [11] = 8,
+    [12] = 3,
+    [13] = 6,
+    [14] = 21,
+    [15] = 1,   -- String to Text
+    [16] = 12,  -- Number to Value
+    [17] = 6,   -- I don't know what to do with Boolean, I guess Variable
+    [18] = 6,   -- I guess Array is also Variable
+    [19] = 6,   -- I guess Object is also Variable
+    [20] = 14,  -- Key to Keyword
+    [21] = 21,  -- Null to Constant
+    [22] = 20,
+    [23] = 23,
+    [24] = 24,
+    [25] = 24,
+    [26] = 25,
+}
+
 source.new = function()
   return setmetatable({}, { __index = source })
 end
@@ -58,6 +124,7 @@ source.complete = function(self, _, callback)
       level = level or 0
       for _, node in ipairs(nodes) do
         local kind_name = SymbolKind[node.kind]
+        local mappedKind = kindMapping[node.kind]
         if vim.tbl_contains({ 'Module', 'Namespace', 'Object', 'Class', 'Interface', 'Method', 'Function' }, kind_name) then
           -- node may be LSP DocumentSymbol or SymbolInformation (deprecated)
           local range = node.selectionRange or node.range or (node.location or {}).range
@@ -68,7 +135,8 @@ source.complete = function(self, _, callback)
               insertText = ('\\%%%sl'):format(range.start.line + 1),
               filterText = '@' .. node.name,
               sortText = '' .. range.start.line,
-              kind = node.kind,
+              --kind = node.kind,
+              kind = mappedKind,
               data = node,
             })
             traverse(node.children or {}, level + 1)
